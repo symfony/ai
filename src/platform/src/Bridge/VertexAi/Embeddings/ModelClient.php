0a614cdae24474137a9c7cf8d359b8dc600c7ffd
junaid farooq <junaid@yarikul.in>
2025-08-11 17:32:50 +0530

    feat(platform): Add support for Google vertex AI - Adds support for embeddings

---
+++ src/platform/src/Bridge/VertexAi/Embeddings/ModelClient.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Bridge\VertexAi\Embeddings;
+
+use Symfony\AI\Platform\Model as BaseModel;
+use Symfony\AI\Platform\ModelClientInterface;
+use Symfony\AI\Platform\Result\RawHttpResult;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * @author Junaid Farooq <ulislam.junaid125@gmail.com>
+ */
+final readonly class ModelClient implements ModelClientInterface
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private string $location,
+        private string $projectId,
+    ) {
+    }
+
+    public function supports(BaseModel $model): bool
+    {
+        return $model instanceof Model;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function request(BaseModel $model, array|string $payload, array $options = []): RawHttpResult
+    {
+        $url = \sprintf(
+            'https://%s-aiplatform.googleapis.com/v1/projects/%s/locations/%s/publishers/google/models/%s:%s',
+            $this->location,
+            $this->projectId,
+            $this->location,
+            $model->getName(),
+            'predict',
+        );
+
+        $modelOptions = $model->getOptions();
+
+        if (isset($modelOptions['task_type']) && $modelOptions['task_type'] instanceof TaskType) {
+            $modelOptions['task_type'] = $modelOptions['task_type']->value;
+        }
+
+        $payload = [
+            'instances' => array_map(
+                static fn (string $text) => [
+                    'content' => ['parts' => [['text' => $text]]],
+                    'title' => $options['title'] ?? null,
+                    'task_type' => isset($modelOptions['task_type']) ? $modelOptions['task_type']->value : null,
+                ],
+                \is_array($payload) ? $payload : [$payload],
+            ),
+        ];
+
+        unset($modelOptions['task_type']);
+
+        return new RawHttpResult(
+            $this->httpClient->request(
+                'POST',
+                $url,
+                [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                    ],
+                    'json' => array_merge($payload, $modelOptions),
+                ]
+            )
+        );
+    }
+}
