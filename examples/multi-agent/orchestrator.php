99ff6ee9844a7293074b35561307bf534767a0c5
Oskar Stark <oskarstark@googlemail.com>
2025-09-15 08:10:22 +0200

    Limit example output to 300 characters for readability

--- examples/multi-agent/orchestrator.php
+++ examples/multi-agent/orchestrator.php
@@ -64,11 +64,11 @@ $messages = new MessageBag(
     Message::ofUser('I get this error in my php code: "Call to undefined method App\Controller\UserController::getName()" - this is my line of code: $user->getName() where $user is an instance of User entity.')
 );
 $result = $multiAgent->call($messages);
-echo $result->getContent().\PHP_EOL.\PHP_EOL;
+echo substr($result->getContent(), 0, 300).'...'.\PHP_EOL.\PHP_EOL;
 
 echo "=== General Question ===\n";
 $messages = new MessageBag(
     Message::ofUser('Can you give me a lasagne recipe?')
 );
 $result = $multiAgent->call($messages);
-echo $result->getContent().\PHP_EOL;
+echo substr($result->getContent(), 0, 300).'...'.\PHP_EOL;
