e4ea423287ac3135394d4d8f1516f4294a295670
Guillaume Loulier <guillaume.loulier@hotmail.fr>
2025-10-21 17:26:08 +0200

    feat(chat): redis added

--- examples/commands/message-stores.php
+++ examples/commands/message-stores.php
@@ -15,8 +15,10 @@ use Symfony\AI\Chat\Bridge\HttpFoundation\SessionStore;
 use Symfony\AI\Chat\Bridge\Local\CacheStore;
 use Symfony\AI\Chat\Bridge\Local\InMemoryStore;
 use Symfony\AI\Chat\Bridge\Meilisearch\MessageStore as MeilisearchMessageStore;
+use Symfony\AI\Chat\Bridge\Redis\MessageStore as RedisMessageStore;
 use Symfony\AI\Chat\Command\DropStoreCommand;
 use Symfony\AI\Chat\Command\SetupStoreCommand;
+use Symfony\AI\Chat\MessageNormalizer;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Clock\MonotonicClock;
 use Symfony\Component\Console\Application;
@@ -27,6 +29,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
 
 $factories = [
     'cache' => static fn (): CacheStore => new CacheStore(new ArrayAdapter(), cacheKey: 'symfony'),
@@ -38,6 +42,14 @@ $factories = [
         'symfony',
     ),
     'memory' => static fn (): InMemoryStore => new InMemoryStore('symfony'),
+    'redis' => static fn (): RedisMessageStore => new RedisMessageStore(new Redis([
+        'host' => env('REDIS_HOST'),
+        'port' => env('REDIS_PORT'),
+    ]), 'symfony', new Serializer([
+        new MessageNormalizer(),
+    ], [
+        new JsonEncoder(),
+    ])),
     'session' => static function (): SessionStore {
         $request = Request::create('/');
         $request->setSession(new Session(new MockArraySessionStorage()));
