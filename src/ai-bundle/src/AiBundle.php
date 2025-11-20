de92aea58e7c351fa36ac5c1c06aca72d19222ad
Grégoire Pineau <lyrixx@lyrixx.info>
2025-11-20 10:44:39 +0100

    [Store] Add a way to bypass document loading in Indexer

--- src/ai-bundle/src/AiBundle.php
+++ src/ai-bundle/src/AiBundle.php
@@ -1744,6 +1744,7 @@ final class AiBundle extends AbstractBundle
             new Reference($config['vectorizer']),
             new Reference($config['store']),
             $config['source'],
+            [],
             $filters,
             $transformers,
             new Reference('logger', ContainerInterface::IGNORE_ON_INVALID_REFERENCE),
