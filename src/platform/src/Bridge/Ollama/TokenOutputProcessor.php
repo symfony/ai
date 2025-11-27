4776a60401b0b22abfa89d08fd6c0b67137093b4
Guillaume Loulier <guillaume.loulier@hotmail.fr>
2025-11-27 17:21:56 +0100

    ref

---
+++ src/platform/src/Bridge/Ollama/TokenOutputProcessor.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Bridge\Ollama;
+
+use Symfony\AI\Agent\Output;
+use Symfony\AI\Agent\OutputProcessorInterface;
+use Symfony\AI\Platform\Metadata\TokenUsage;
+use Symfony\AI\Platform\Result\StreamResult;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+/**
+ * @author Guillaume Loulier <personal@guillaumeloulier.fr>
+ */
+final class TokenOutputProcessor implements OutputProcessorInterface
+{
+    public function processOutput(Output $output): void
+    {
+        $result = $output->getResult();
+        $currentOutputMetadata = $result->getMetadata();
+
+        if ($result instanceof StreamResult) {
+            foreach ($result->getContent() as $chunk) {
+                dump($chunk);
+            }
+        }
+
+        $rawResponse = $result->getRawResult()?->getObject();
+        if (!$rawResponse instanceof ResponseInterface) {
+            return;
+        }
+
+        $payload = $rawResponse->toArray(false);
+
+        $currentOutputMetadata->add('token_usage', new TokenUsage(
+            promptTokens: $payload['prompt_eval_count'],
+            completionTokens: $payload['eval_count']
+        ));
+    }
+}
