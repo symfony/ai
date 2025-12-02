7076c55c43437e7afa5ec051f3225e41ee3f0280
Guillaume Loulier <guillaume.loulier@hotmail.fr>
2025-12-02 09:20:29 +0100

    ref

---
+++ examples/ollama/agent-with-token-processor-as-stream.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Agent\Agent;
+use Symfony\AI\Platform\Bridge\Ollama\PlatformFactory;
+use Symfony\AI\Platform\Bridge\Ollama\TokenOutputProcessor;
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = PlatformFactory::create(env('OLLAMA_HOST_URL'), http_client());
+
+$agent = new Agent($platform, env('OLLAMA_LLM'), outputProcessors: [new TokenOutputProcessor()]);
+$answer = $agent->call(new MessageBag(
+    Message::forSystem('You are a helpful assistant.'),
+    Message::ofUser('Tina has one brother and one sister. How many sisters do Tina\'s siblings have?'),
+), ['stream' => true]);
+
+foreach ($answer->getContent() as $content) {
+    dd($content);
+    echo $content . \PHP_EOL;
+}
+
+
