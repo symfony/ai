d8e3b5128f8cc5fd53f4cc519f2b435055976502
Oskar Stark <oskarstark@googlemail.com>
2025-12-09 23:38:33 +0100

    [Platform] Remove PHPDoc type annotations from FileNormalizerTest

--- src/platform/src/Bridge/HuggingFace/Tests/Contract/FileNormalizerTest.php
+++ src/platform/src/Bridge/HuggingFace/Tests/Contract/FileNormalizerTest.php
@@ -41,9 +41,6 @@ final class FileNormalizerTest extends TestCase
         $this->assertSame($expected, $normalizer->getSupportedTypes(null));
     }
 
-    /**
-     * @param array<string, mixed> $expected
-     */
     #[DataProvider('normalizeDataProvider')]
     public function testNormalize(File $file, array $expected)
     {
@@ -54,9 +51,6 @@ final class FileNormalizerTest extends TestCase
         $this->assertEquals($expected, $normalized);
     }
 
-    /**
-     * @return iterable<array{0: File, 1: array<string, mixed>}>
-     */
     public static function normalizeDataProvider(): iterable
     {
         yield 'image from file' => [
