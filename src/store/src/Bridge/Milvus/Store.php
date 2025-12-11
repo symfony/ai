a71d7bfa35e100de65a73bf001952efe11dcac3f
Diego Aguiar <barret91@hotmail.com>
2025-12-11 13:07:59 -0700

    Remove Uuid auto-casting from all stores

--- src/store/src/Bridge/Milvus/Store.php
+++ src/store/src/Bridge/Milvus/Store.php
@@ -155,7 +155,7 @@ final class Store implements ManagedStoreInterface, StoreInterface
     private function convertToIndexableArray(VectorDocument $document): array
     {
         return [
-            'id' => $document->id->toRfc4122(),
+            'id' => $document->id,
             '_metadata' => json_encode($document->metadata->getArrayCopy()),
             $this->vectorFieldName => $document->vector->getData(),
         ];
@@ -174,6 +174,6 @@ final class Store implements ManagedStoreInterface, StoreInterface
 
         $score = $data['distance'] ?? null;
 
-        return new VectorDocument(Uuid::fromString($id), $vector, new Metadata(json_decode($data['_metadata'], true)), $score);
+        return new VectorDocument($id, $vector, new Metadata(json_decode($data['_metadata'], true)), $score);
     }
 }
