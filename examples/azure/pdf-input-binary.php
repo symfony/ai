f8a4c9e4049a781bb745ba440f41311e4a5c4ca3
Christopher Hertel <mail@christopher-hertel.de>
2026-03-04 23:55:57 +0100

    Streamline Open Responses PDF support on Azure and OpenAI

---
+++ examples/azure/pdf-input-binary.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Platform\Bridge\Azure\OpenAi\PlatformFactory;
+use Symfony\AI\Platform\Message\Content\Document;
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = PlatformFactory::create(
+    env('AZURE_OPENAI_BASEURL'),
+    env('AZURE_OPENAI_GPT_DEPLOYMENT'),
+    env('AZURE_OPENAI_GPT_API_VERSION'),
+    env('AZURE_OPENAI_KEY'),
+    http_client(),
+);
+
+$messages = new MessageBag(
+    Message::ofUser(
+        'What is this document about?',
+        Document::fromFile(dirname(__DIR__, 2).'/fixtures/document.pdf'),
+    ),
+);
+$result = $platform->invoke('gpt-5-mini', $messages);
+
+echo $result->asText().\PHP_EOL;
