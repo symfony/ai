fa364e9e7de0d50384b596a4be57bc9faf248c1f
Guillaume Loulier <guillaume.loulier@hotmail.fr>
2026-03-13 13:15:55 +0100

    refactor(platform): add support for generation endpoint on Ollama

---
+++ examples/ollama/text-generation-as-stream.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Platform\Bridge\Ollama\Factory;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = Factory::createPlatform(env('OLLAMA_HOST_URL'), env('OLLAMA_API_KEY'), httpClient: http_client());
+
+$result = $platform->invoke(env('OLLAMA_LLM'), 'Tina has one brother and one sister. How many sisters do Tina\'s siblings have?', [
+    'stream' => true,
+]);
+
+foreach ($result->asTextStream() as $word) {
+    echo $word;
+}
+echo \PHP_EOL;
