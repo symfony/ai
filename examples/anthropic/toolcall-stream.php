0a83640b2d5ec6059611090d0d2d81c1b01ec256
Christopher Hertel <mail@christopher-hertel.de>
2026-04-06 23:01:46 +0200

    Add ThinkingStart and extend Anthropic and OpenAI example

--- examples/anthropic/toolcall-stream.php
+++ examples/anthropic/toolcall-stream.php
@@ -17,6 +17,9 @@ use Symfony\AI\Platform\Bridge\Anthropic\PlatformFactory;
 use Symfony\AI\Platform\Message\Message;
 use Symfony\AI\Platform\Message\MessageBag;
 use Symfony\AI\Platform\Result\Stream\Delta\TextDelta;
+use Symfony\AI\Platform\Result\Stream\Delta\ThinkingComplete;
+use Symfony\AI\Platform\Result\Stream\Delta\ThinkingDelta;
+use Symfony\AI\Platform\Result\Stream\Delta\ThinkingStart;
 
 require_once dirname(__DIR__).'/bootstrap.php';
 
@@ -33,9 +36,22 @@ $messages = new MessageBag(Message::ofUser(<<<TXT
     TXT));
 $result = $agent->call($messages, [
     'stream' => true, // enable streaming of response text
+    'thinking' => [
+        'type' => 'enabled',
+        'budget_tokens' => 10000,
+    ],
 ]);
 
 foreach ($result->getContent() as $delta) {
+    if ($delta instanceof ThinkingStart) {
+        output()->writeln('<info><thinking></info>');
+    }
+    if ($delta instanceof ThinkingDelta) {
+        output()->write('<fg=#999999>'.$delta->getThinking().'</>');
+    }
+    if ($delta instanceof ThinkingComplete) {
+        output()->writeln(\PHP_EOL.'<info></thinking></info>');
+    }
     if ($delta instanceof TextDelta) {
         echo $delta;
     }
