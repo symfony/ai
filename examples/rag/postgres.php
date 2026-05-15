Guikingone/symfony-ai

fix(postgres): fix on stored lang + StoreFactory

examples/rag/postgres.php
@@ -19,7 +19,7 @@ use Symfony\AI\Fixtures\Movies;
 use Symfony\AI\Platform\Bridge\OpenAi\Factory;
 use Symfony\AI\Platform\Message\Message;
 use Symfony\AI\Platform\Message\MessageBag;
-use Symfony\AI\Store\Bridge\Postgres\Store;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
 use Symfony\AI\Store\Document\Metadata;
 use Symfony\AI\Store\Document\TextDocument;
 use Symfony\AI\Store\Document\Vectorizer;
@@ -31,7 +31,7 @@ use Symfony\Component\Uid\Uuid;
 require_once dirname(__DIR__).'/bootstrap.php';
 
 // initialize the store
-$store = Store::fromDbal(
+$store = StoreFactory::createFromDbal(
     connection: DriverManager::getConnection((new DsnParser())->parse(env('POSTGRES_URI'))),
     tableName: 'my_table',
 );

symfony/ai

examples/rag/postgres.php
@@ -19,7 +19,7 @@ use Symfony\AI\Fixtures\Movies;
 use Symfony\AI\Platform\Bridge\OpenAi\Factory;
 use Symfony\AI\Platform\Message\Message;
 use Symfony\AI\Platform\Message\MessageBag;
-use Symfony\AI\Store\Bridge\Postgres\Store;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
 use Symfony\AI\Store\Document\Metadata;
 use Symfony\AI\Store\Document\TextDocument;
 use Symfony\AI\Store\Document\Vectorizer;
@@ -31,7 +31,7 @@ use Symfony\Component\Uid\Uuid;
 require_once dirname(__DIR__).'/bootstrap.php';
 
 // initialize the store
-$store = Store::fromDbal(
+$store = StoreFactory::createStoreFromDbal(
     connection: DriverManager::getConnection((new DsnParser())->parse(env('POSTGRES_URI'))),
     tableName: 'my_table',
 );