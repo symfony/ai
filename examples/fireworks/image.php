Guikingone/symfony-ai

[Platform] Add Fireworks AI bridge
Add a dedicated Platform bridge for Fireworks AI covering chat completions,
embeddings, reranking and image generation, with a dynamic ModelCatalog backed
by the gateway list-models API and a static overlay for the image and rerank
models the gateway does not flag.

- Bridge package symfony/ai-fireworks-platform: Fireworks Model marker, Factory
  (Ollama-style, builds the catalog internally), FireworksClient and
  FireworksResultConverter routed by capability/URL, TokenUsageExtractor and a
  dynamic ModelCatalog with pagination and capability detection
- ai-bundle wiring: fireworks config node, options import, AiBundle handler
  block, ModelCatalog service, CHANGELOG entry and guard tests
- Documentation: ai-bundle.rst Fireworks section and platform.rst model/example
  references
- Examples: chat, stream, toolcall, structured-output, embeddings, rerank, image
- platform composer.json: add the "fireworks" keyword

examples/fireworks/image.php
@@ -1,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Platform\Bridge\Fireworks\Factory;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = Factory::createPlatform(env('FIREWORKS_API_KEY'), http_client());
+
+$result = $platform->invoke('accounts/fireworks/models/flux-1-schnell-fp8', 'A cat in a space suit floating in front of the moon', [
+    'aspect_ratio' => '16:9',
+]);
+
+$image = $result->asBinary();
+
+$path = sys_get_temp_dir().'/fireworks-image.png';
+$image->asFile($path);
+
+output()->writeln(sprintf('Image saved to %s', $path));

examples/fireworks/rerank.php
@@ -1,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Platform\Bridge\Fireworks\Factory;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = Factory::createPlatform(env('FIREWORKS_API_KEY'), http_client());
+
+$result = $platform->invoke('accounts/fireworks/models/qwen3-reranker-8b', [
+    'query' => 'What is machine learning?',
+    'documents' => [
+        'Machine learning is a field of study that gives computers the ability to learn without being explicitly programmed.',
+        'Cooking is the art of preparing food with heat.',
+        'Deep learning is a subset of machine learning based on artificial neural networks.',
+        'The weather today is sunny with a light breeze.',
+    ],
+]);
+
+$reranking = $result->asReranking();
+foreach ($reranking->getContent() as $entry) {
+    output()->writeln(sprintf('Document %d: relevance score %.4f', $entry->getIndex(), $entry->getScore()));
+}

symfony/ai

examples/fireworks/image.php
@@ -1,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Platform\Bridge\Fireworks\Factory;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = Factory::createPlatform(env('FIREWORKS_API_KEY'), http_client());
+
+$result = $platform->invoke('accounts/fireworks/models/flux-1-schnell-fp8', 'A cat in a space suit floating in front of the moon', [
+    'aspect_ratio' => '16:9',
+]);
+
+$path = sys_get_temp_dir().'/fireworks-image.png';
+$result->asFile($path);
+
+output()->writeln(sprintf('Image saved to %s', $path));

examples/fireworks/rerank.php
@@ -1,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Platform\Bridge\Fireworks\Factory;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = Factory::createPlatform(env('FIREWORKS_API_KEY'), http_client());
+
+$result = $platform->invoke('accounts/fireworks/models/qwen3-reranker-8b', [
+    'query' => 'What is machine learning?',
+    'documents' => [
+        'Machine learning is a field of study that gives computers the ability to learn without being explicitly programmed.',
+        'Cooking is the art of preparing food with heat.',
+        'Deep learning is a subset of machine learning based on artificial neural networks.',
+        'The weather today is sunny with a light breeze.',
+    ],
+]);
+
+foreach ($result->asReranking() as $entry) {
+    output()->writeln(sprintf('Document %d: relevance score %.4f', $entry->getIndex(), $entry->getScore()));
+}